f62b6e326daff4b8d9335eca1c5579677bf2a214
Parikshit <parikshitgupta2001@gmail.com>
2025-06-04 17:47:38 +0530

    custom React

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+const anotherUser = " | chai aur React"
+
+const reactElement = React.createElement(
+  'a',
+  {'href': 'https://google.com', 'target': '_blank'},
+  'Click Me to Visit Google',
+  anotherUser
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  reactElement
 )
